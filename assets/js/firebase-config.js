869f7dcac8ac1f6a11e5afd170ab89eb1f2285f6
Bruno Otávio Jeronimo Melo <bruno.otavio.jm@outlook.com>
2026-09-01 11:06:46 -0300

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNI763YxZS0cb6Qu1DAQbjR7ianOq0M_w",
-  authDomain: "manualbtc-8c8a0.firebaseapp.com",
-  projectId: "manualbtc-8c8a0",
-  storageBucket: "manualbtc-8c8a0.firebasestorage.app",
-  messagingSenderId: "257113552913",
-  appId: "1:257113552913:web:30a329919bc4cedd7219b0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
